1e2537cc5a79b712937877b51627b83dc33294fe
kimvykz <kimslavakz@gmail.com>
2024-07-22 12:44:05 +0900

    1-48-22 checkpoint

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import MealsByName from '../views/MealsByName.vue';
 import MealsByIngredient from '../views/MealsByIngredient.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
 import GuestLayout from '../components/GuestLayout.vue';
+import MealDetails from '../views/MealDetails.vue';
 
 const routes = [
   {
@@ -32,6 +33,11 @@ const routes = [
         name: 'byIngredient',
         component: MealsByIngredient
       },
+      {
+        path: '/meal/:id',
+        name: 'mealDetails',
+        component: MealDetails
+      }
     ]
   },
   {
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-8">
+  <div class="p-8 pb-0">
     <input 
       type="text" 
       v-model="keyword"
@@ -11,14 +11,14 @@
 
   <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
     <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
-      <router-link to="/">
+      <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
         <img :src="meal.strMealThumb" :alt="strMeal" class="rounded-t-xl w-full h-48 object-cover">
       </router-link>
       <div class="p-3">
         <h3 class="font-bold">{{ meal.strMeal }}</h3>
         <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quis temporibus fugit, iure animi iste rem quod dolore odio sint aliquid sapiente alias ab libero magni quas nobis id laudantium?</p>
         <div class="flex items-center justify-between">
-          <a :href="meal.strYoutube" target="_blank" class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors">YouTube</a> 
+          <YouTubeButton :href="meal.strYoutube" /> 
           
         </div>
       </div>
@@ -30,6 +30,7 @@
 import { ref, computed, onMounted } from 'vue';
 import store from '../store';
 import { useRoute } from 'vue-router';
+import YouTubeButton from '../components/YouTubeButton.vue';
 
 const route = useRoute();
 const keyword = ref('');
